729b2807263335086ea62f5b9fda8e8cdde28c77
GenoSamuel <truegenosamuel@gmail.com>
2022-02-22 09:26:00 +0900

    added stylings and dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,105 @@
 import React from 'react';
+import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import 'tachyons';
 import './App.css';
 
+  
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
+      
       <Navigation />
+      <Particles className='particles'
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 24,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 200,
+              duration: 10,
+              opacity: 0.8,
+              size: 10,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 20,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 1,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
       <Logo />
+      <Rank />
       <ImageLinkForm />
          {/*
       <FaceRecognition />*/}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
     return (
         <div>
-            <p className="f3">
+            <p className="f3 white">
                 {'I can detect faces in any picture. Try me!'}
             </p>
             <div className="center">
-                <div className="form center pa4 br3 shadow-5">
+                <div className="form center pa4 br3 shadow-3">
                     <input className="f4 pa2 w-70 center " type="text" />
                     <button className="w-30 grow f4 link ph3 pv2 dib white bg-blue">Detect</button>
                 </div>
